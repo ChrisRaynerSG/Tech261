5904327d24b1c031e0a641e32230ba501f14b572
Chris Rayner <crayner@spartaglobal.com>
2024-06-17 16:24:27 +0100

    Added extra unit tests to ProgramTest and modified Program to display times differently

--- JavaBasics/src/test/java/com/sparta/cr/ProgramTest.java
+++ JavaBasics/src/test/java/com/sparta/cr/ProgramTest.java
@@ -1,11 +1,56 @@
 package com.sparta.cr;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProgramTest {
-    @Test
+    @Test //Annotation carries Metadata. Passes this to JVM
+    @DisplayName("This is my first test")
     public void basicTest(){
         Assertions.assertTrue(5==5);
+    }
+    @Test
+    @DisplayName("Given a time of 21, then the greeting should be Good Evening")
+    void checkThat21GivesGoodEvening(){
+        int time =21;
+        String expectedGreeting = "Good evening!";
+        String actualGreeting = Program.getGreeting(time);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    @Test
+    @DisplayName("Given a time of 12, the greeting should be Good afternoon!")
+    void checkThat12GivesGoodAfternoon(){
+        int time = 12;
+        String expectedGreeting = "Good afternoon!".toLowerCase();
+        String actualGreeting = Program.getGreeting(time).toLowerCase();
+        Assertions.assertEquals(expectedGreeting,actualGreeting);
+    }
+    @Test
+    @DisplayName("Given a valid time an output is displayed")
+    void checkThatAllValidTimesHaveOutput(){
+        boolean validOutput = true;
+        for(int i = 0; i<23; i++){
+            if(Program.getGreeting(i).isEmpty()){
+                validOutput = false;
+            }
+            Assertions.assertTrue(validOutput);
+        }
+    }
+    @Test
+    @DisplayName("Given an invalid time message output should reflect that")
+    void checkThatInvalidTimeOutputCorrect(){
+        int time = 25;
+        String expectedGreeting = "invalid time of day".toLowerCase();
+        String actualGreeting = Program.getGreeting(time).toLowerCase();
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    @Test
+    @DisplayName("Given a negative time message output should reflect that")
+    void checkForNegativeNumberOutput(){
+        int time = -1;
+        String expectedGreeting = "invalid time of day".toLowerCase();
+        String actualGreeting = Program.getGreeting(time).toLowerCase();
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
     }
 }
