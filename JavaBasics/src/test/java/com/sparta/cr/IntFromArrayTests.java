877e08b62d5f97d6a0ba84ae75b27a5c8aba4f89
Chris Rayner <crayner@spartaglobal.com>
2024-06-18 19:38:46 +0100

    Added new unit test to check negative ints within the int array work as intended

--- JavaBasics/src/test/java/com/sparta/cr/IntFromArrayTests.java
+++ JavaBasics/src/test/java/com/sparta/cr/IntFromArrayTests.java
@@ -68,6 +68,20 @@ public class IntFromArrayTests {
         Assertions.assertEquals(numberExpected,actualInt);
     }
 
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("Given an array of integers including negative integers getIntFromArray returns single int with no negative signs")
+    public void GivenArrayOfIntsWithNegativesReturnSingleIntWithArrayContents(int[] arrayInput, int numberExpected){
+        //Arrange
+        //Done in Method Source
+
+        //Act
+        int actualInt = IntFromArray.getIntFromArray(arrayInput);
+
+        //Assert
+        Assertions.assertEquals(numberExpected,actualInt);
+    }
+
     public static Stream<Arguments> GivenArrayOfIntsReturnSingleIntWithArrayContents(){
         return Stream.of(
                 Arguments.of(new int[]{1,4,5,7}, 1457),
@@ -81,6 +95,13 @@ public class IntFromArrayTests {
                 Arguments.of(new int[]{0,0,1,4,5}, 145),
                 Arguments.of(new int[]{0,0,3,0,5,0}, 3050),
                 Arguments.of(new int[]{0,0,0,0,0,0}, 0)
+        );
+    }
+    public static Stream<Arguments> GivenArrayOfIntsWithNegativesReturnSingleIntWithArrayContents(){
+        return Stream.of(
+                Arguments.of(new int[]{1,-4,5,-7}, 1457),
+                Arguments.of(new int[]{2,-5,7,12,-5},257125),
+                Arguments.of(new int[]{-1,5,-2,-1,-2},15212)
         );
     }
 }
