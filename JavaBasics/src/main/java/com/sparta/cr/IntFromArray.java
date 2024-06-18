531416f8c7b953e3a38ffbe3f2080fa314de586e
Chris Rayner <crayner@spartaglobal.com>
2024-06-18 17:30:54 +0100

    Created IntFromArrayTests Unit Tests and IntFromArray Class. Created Unit tests to check code written in IntFromArray. Should have done this in multiple commits and will try to do that in the future.

---
+++ JavaBasics/src/main/java/com/sparta/cr/IntFromArray.java
@@ -0,0 +1,20 @@
+package com.sparta.cr;
+
+public class IntFromArray {
+
+    public static int getIntFromArray(int[] arrayToTest){
+        String finalIntegerString ="";
+        int finalInt = 0;
+        if(arrayToTest.length==0){
+            return finalInt;
+        }
+
+        for (int i = 0; i<arrayToTest.length; i++){
+            String integerToAdd;
+            integerToAdd = Integer.toString(arrayToTest[i]);
+            finalIntegerString += integerToAdd;
+        }
+        finalInt = Integer.parseInt(finalIntegerString);
+        return finalInt;
+    }
+}
---
+++ JavaBasics/src/test/java/com/sparta/cr/IntFromArrayTests.java
@@ -0,0 +1,86 @@
+package com.sparta.cr;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
+public class IntFromArrayTests {
+
+    @Test
+    @DisplayName("Given an empty array of integers, then 0 should be returned")
+    void GivenEmptyArrayReturnsZero(){
+
+        //Arrange
+        int[] arrayToTest = {};
+        int expectedInt = 0;
+
+        //Act
+        int actualInt = IntFromArray.getIntFromArray(arrayToTest);
+
+        //Assert
+        Assertions.assertEquals(expectedInt,actualInt);
+    }
+
+    @Test
+    @DisplayName("Given an array of integers, then an integer from that array should be returned ")
+    void GivenArrayOfIntsReturnSingleInt(){
+
+        //Arrange
+        int[] arrayToTest = {1};
+        int expectedInt = 1;
+
+        //Act
+        int actualInt = IntFromArray.getIntFromArray(arrayToTest);
+
+        //Assert
+        Assertions.assertEquals(expectedInt,actualInt);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("Given an array of integers, then a single int displaying all the integers in the array should be returned ")
+    public void GivenArrayOfIntsReturnSingleIntWithArrayContents(int[] arrayInput, int numberExpected){
+
+        //Arrange
+        //Done in method source
+
+        //Act
+        int actualInt = IntFromArray.getIntFromArray(arrayInput);
+
+        //Assert
+        Assertions.assertEquals(numberExpected,actualInt);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("Given an array of integers which starts with 0's return a single int dropping the leading 0's but keeps any subsequent 0's")
+    public void GivenArrayOfIntsReturnSingleIntWithNoLeadingZeros(int[] arrayInput, int numberExpected){
+        //Arrange
+        //Done in Method source
+
+        //Act
+        int actualInt = IntFromArray.getIntFromArray(arrayInput);
+
+        //Assert
+        Assertions.assertEquals(numberExpected,actualInt);
+    }
+
+    public static Stream<Arguments> GivenArrayOfIntsReturnSingleIntWithArrayContents(){
+        return Stream.of(
+                Arguments.of(new int[]{1,4,5,7}, 1457),
+                Arguments.of(new int[]{2,5,7,12,5},257125),
+                Arguments.of(new int[]{1,5,2,1,2},15212)
+        );
+    }
+
+    public static Stream<Arguments> GivenArrayOfIntsReturnSingleIntWithNoLeadingZeros(){
+        return Stream.of(
+                Arguments.of(new int[]{0,0,1,4,5}, 145),
+                Arguments.of(new int[]{0,0,3,0,5,0}, 3050),
+                Arguments.of(new int[]{0,0,0,0,0,0}, 0)
+        );
+    }
+}
