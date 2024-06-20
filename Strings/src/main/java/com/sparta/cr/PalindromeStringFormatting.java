3cc35129eaddf47ad0bbc23a99fbca6c060a98f3
Chris Rayner <crayner@spartaglobal.com>
2024-06-20 16:14:14 +0100

    added getArrayOfIndividualWords method to PalindromeStringFormatting. Added tests and made functional

--- Strings/src/main/java/com/sparta/cr/PalindromeStringFormatting.java
+++ Strings/src/main/java/com/sparta/cr/PalindromeStringFormatting.java
@@ -1,13 +1,16 @@
 package com.sparta.cr;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PalindromeStringFormatting {
     static final String ACCEPTABLE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz ";
 
-    static String stripSpecialCharacters(String inputString){
+    static String getStripSpecialCharacters(String inputString){
         StringBuilder outputString = new StringBuilder();
-        if (inputString == null || inputString.isEmpty())
+        if (inputString == null || inputString.isEmpty()) {
             return outputString.toString();
-
+        }
+        inputString = inputString.toLowerCase();
         for(int inputStringIndex = 0; inputStringIndex < inputString.length(); inputStringIndex++){
             for (int acceptableCharactersIndex = 0; acceptableCharactersIndex < ACCEPTABLE_CHARACTERS.length(); acceptableCharactersIndex++){
                 if(inputString.charAt(inputStringIndex) == ACCEPTABLE_CHARACTERS.charAt(acceptableCharactersIndex)){
@@ -17,5 +20,32 @@ public class PalindromeStringFormatting {
             }
         }
         return outputString.toString();
+    }
+
+    static List<String> getArrayOfIndividualWords(String inputString){
+        StringBuilder singleWord = new StringBuilder();
+        List<String> outputStringArrayList = new ArrayList<String>();
+
+        if (inputString == null || inputString.isEmpty()) {
+            return outputStringArrayList;
+        }
+
+        inputString = getStripSpecialCharacters(inputString);
+
+        for(int inputStringIndex = 0; inputStringIndex < inputString.length(); inputStringIndex++){
+            if(inputString.charAt(inputStringIndex) != ' '){
+                singleWord.append(inputString.charAt(inputStringIndex));
+                if(!singleWord.isEmpty() && inputStringIndex == inputString.length()-1){
+                    outputStringArrayList.add(singleWord.toString());
+                }
+            }
+            else{
+                if (!singleWord.isEmpty()) {
+                    outputStringArrayList.add(singleWord.toString());
+                }
+                singleWord.setLength(0);
+            }
+        }
+        return outputStringArrayList;
     }
 }
--- Strings/src/test/java/com/sparta/cr/PalindromeStringFormattingTests.java
+++ Strings/src/test/java/com/sparta/cr/PalindromeStringFormattingTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class PalindromeStringFormattingTests {
@@ -17,7 +19,7 @@ public class PalindromeStringFormattingTests {
         String expected = "";
 
         //Act
-        String actual = PalindromeStringFormatting.stripSpecialCharacters(input);
+        String actual = PalindromeStringFormatting.getStripSpecialCharacters(input);
 
         //Assert
         Assertions.assertEquals(expected,actual);
@@ -29,7 +31,7 @@ public class PalindromeStringFormattingTests {
         String expected = "";
 
         //Act
-        String actual = PalindromeStringFormatting.stripSpecialCharacters(input);
+        String actual = PalindromeStringFormatting.getStripSpecialCharacters(input);
 
         //Assert
         Assertions.assertEquals(expected, actual);
@@ -41,10 +43,34 @@ public class PalindromeStringFormattingTests {
         //MethodSource
 
         //Act
-        String actual = PalindromeStringFormatting.stripSpecialCharacters(input);
+        String actual = PalindromeStringFormatting.getStripSpecialCharacters(input);
 
         //Assert
         Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void givenStringReturnStringWithNoSpecialCharactersAndAllLowerCase(String input, String expected){
+        //Arrange
+        //MethodSource
+
+        //Act
+        String actual = PalindromeStringFormatting.getStripSpecialCharacters(input);
+
+        //Assert
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void givenStringReturnStringListOfEachIndividualWord(String input, List expected){
+
+        //Act
+        List<String> actual = new ArrayList<>(PalindromeStringFormatting.getArrayOfIndividualWords(input));
+
+        //Assert
+        Assertions.assertEquals(expected,actual);
     }
 
     static Stream<Arguments> givenStringReturnStringWithNoSpecialCharacters(){
@@ -53,6 +79,29 @@ public class PalindromeStringFormattingTests {
                 Arguments.of("he!!llo i am a str!ng", "hello i am a strng"),
                 Arguments.of("!!%&$*(£", ""),
                 Arguments.of("hello !&^**&^", "hello ")
+        );
+    }
+
+    static Stream<Arguments> givenStringReturnStringWithNoSpecialCharactersAndAllLowerCase(){
+        return Stream.of(
+                Arguments.of("Hello i am A string", "hello i am a string"),
+                Arguments.of("He!!llo i aM a stR!ng", "hello i am a strng"),
+                Arguments.of("!!%&$*(£", ""),
+                Arguments.of("heLLo !&^**&^", "hello ")
+        );
+    }
+
+    static Stream<Arguments> givenStringReturnStringListOfEachIndividualWord(){
+        List example1 = new ArrayList(List.of("hello","i","am","a","string"));
+        List example2 = new ArrayList(List.of("hello","i","am","a","strng"));
+        List example3 = new ArrayList(List.of(""));
+        List example4 = new ArrayList(List.of("hello"));
+
+        return Stream.of(
+                Arguments.of("hello i am a string", example1),
+                Arguments.of("he!!Llo i am a str!ng", example2),
+                Arguments.of("!!%&$*(£", example3),
+                Arguments.of("helLo !&^**&^", example4)
         );
     }
 }
