2b2c56df5ff06dbb560da860d1f916ebba17bc6b
Chris Rayner <crayner@spartaglobal.com>
2024-06-21 07:12:26 +0100

    small refactoring of ArrayCheckTests

--- ThreeIdenticalNumbers/src/test/java/com/sparta/cr/ArrayCheckTests.java
+++ ThreeIdenticalNumbers/src/test/java/com/sparta/cr/ArrayCheckTests.java
@@ -1,12 +1,10 @@
 package com.sparta.cr;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.stream.Stream;
 
 public class ArrayCheckTests {
