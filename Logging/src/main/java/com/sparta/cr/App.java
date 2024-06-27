c070a42ce7c688191628c131d85a2a007a135012
Chris Rayner <crayner@spartaglobal.com>
2024-06-27 10:21:04 +0100

    logging examples

---
+++ Logging/src/main/java/com/sparta/cr/App.java
@@ -0,0 +1,38 @@
+package com.sparta.cr;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.*;
+
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+    public static void main(String[] args){
+
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.WARNING);
+        consoleHandler.setFormatter(new CustomFormatter());
+        logger.addHandler(consoleHandler);
+
+        //log file should not be part of the .gitignore
+
+        try {
+            FileHandler fileHandler = new FileHandler("src/main/resources/log-file.log", true);
+            fileHandler.setLevel(Level.ALL);
+            fileHandler.setFormatter(new CustomFormatter());
+            logger.addHandler(fileHandler);
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+
+        //todo something here
+
+        //logger.log(Level.INFO, "this is an info message");
+        logger.setLevel(Level.ALL);
+        logger.setUseParentHandlers(false);
+        logger.info("This is an info message");
+        logger.warning("This is a warning message");
+        logger.fine("This is a fine message");
+
+    }
+}
